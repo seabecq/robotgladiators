1f28698ba06de4b2dc20ac107fe3eb0a21335443
Charlie Pines <charlesbec8@gmail.com>
2022-03-09 12:09:27 -0600

    improved fight or skip loop

--- assets/js/game.js
+++ assets/js/game.js
@@ -7,25 +7,32 @@ var randomNumber = function(min, max) {
   return value;
 };
 
+var fightOrSkip = function() {
+  //ask player if theyd like to fight or skip 
+  var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or ' SKIP' to choose.");
+
+  if (promptFight === "" || promptFight === null ) {
+    window.alert("You need to provide a valid answer! Please try again.");
+    return fightOrSkip();
+  }
+
+  promptFight = promptFight.toLowerCase();
+  
+  if (promptFight === "skip") {
+    var confirmSkip = window.confirm("Are you sure you'd like to skip?");
+    if (confirmSkip) {
+      window.alert(playerInfo.name + " has decided to skip this fight. Goodbye!");
+      playerInfo.money = playerInfo.money - 10;
+      return true;
+    }
+  } return false;
+}
+
 // fight function (now with parameter for enemy's object holding name, health, and attack values)
 var fight = function(enemy) {
   while (playerInfo.health > 0 && enemy.health > 0) {
-    // ask player if they'd like to fight or run
-    var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
-
-    // if player picks "skip" confirm and then stop the loop
-    if (promptFight === "skip" || promptFight === "SKIP") {
-      // confirm player wants to skip
-      var confirmSkip = window.confirm("Are you sure you'd like to quit?");
-
-      // if yes (true), leave fight
-      if (confirmSkip) {
-        window.alert(playerInfo.name + ' has decided to skip this fight. Goodbye!');
-        // subtract money from playerInfo.money for skipping
-        playerInfo.money = Math.max(0, playerInfo.money - 10);
-        console.log("playerInfo.money", playerInfo.money)
-        break;
-      }
+    if (fightOrSkip ()) {
+      break;
     }
 
     // generate random damage value based on player's attack power
@@ -175,6 +182,8 @@ var getPlayerName = function() {
   while (name === "" || name === null) {
     name = prompt("What is your robot's name?");
   }
+  console.log("Your robot's name is " + name);
+  return name;
 };
 
 
